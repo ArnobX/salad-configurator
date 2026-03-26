75f5eb4a9c116a1cbbc375eab863cd55a4e4ef85
Anush707 <anushgotame333@gmail.com>
2026-03-26 20:19:54 +0200

    Task 1.4: Add PriceListItem

--- src/types/index.ts
+++ src/types/index.ts
@@ -24,12 +24,26 @@ export interface Ingredient extends BaseType {
   weight_grams?: number;
 }
 
+export interface User{
+    id: number;
+    email: string;
+    name?: string;
+    role: string;
+}
+
+export interface Recipe{
+    id: number;
+    userId: number;
+    name: string;
+    bowlId: number;
+    IngredientIds: number[];
+    slots?: Record<string, Ingredient | null>;
+    is_public?: boolean;
+}
 
 export interface PriceListItem {
   id: number;
   item_id: number;
   price: number;
-  type?: string;  // optional
+  type?: string;
 }
-
-
