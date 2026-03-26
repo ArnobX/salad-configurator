fe8e740840f94812970fcb04070262647fd0296d
Anush707 <anushgotame333@gmail.com>
2026-03-26 23:00:04 +0200

    task 1.6: Adding Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,31 @@
+export function Footer() {
+  return (
+    <footer className="bg-[#A2D135] text-black p-8 mt-12 w-full flex flex-wrap justify-around items-start">
+      
+      
+      <div>
+        <h3 className="font-bold text-lg mb-2">Contact</h3>
+        <p>Fresh Food Factory</p>
+        <p>Helsinki, Finland</p>
+        <p>info@freshfood.fi</p>
+      </div>
+
+     
+      <div>
+        <h3 className="font-bold text-lg mb-2">Social Media</h3>
+        <p>Instagram</p>
+        <p>Facebook</p>
+        <p>Twitter</p>
+      </div>
+
+      
+      <div>
+        <h3 className="font-bold text-lg mb-2">Fresh Food Factory</h3>
+        <p>FRESSE</p>
+        <p>BOWL-LASKURI</p>
+        <p>© 2024</p>
+      </div>
+
+    </footer>
+  );
+}
