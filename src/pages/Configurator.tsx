c25bb5031e940162b57a11e5f1f89f9dc0273e04
Anush707 <anushgotame333@gmail.com>
2026-04-02 11:35:59 +0300

    task2.10: mapping bowls dynamically

--- src/pages/Configurator.tsx
+++ src/pages/Configurator.tsx
@@ -41,7 +41,7 @@ export default function Configurator() {
   return (
     <main className="flex-1 max-w-6xl w-full mx-auto p-6 flex flex-col gap-8 mt-4">
       <div className="flex flex-col lg:flex-row gap-6 justify-between items-stretch">
-        <BowlSelection />
+        <BowlSelection bowls={bowls} />
         <CenterBowl />
         <BaseSelection />
       </div>
