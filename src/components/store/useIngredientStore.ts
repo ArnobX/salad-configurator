40ceacf7d94dcdc1106a5594ffb2913e71b00e40
Anush707 <anushgotame333@gmail.com>
2026-04-10 11:33:19 +0300

    installing zustand and added basic setter function and empty placeholder

---
+++ src/components/store/useIngredientStore.ts
@@ -0,0 +1,33 @@
+import { create } from "zustand";
+import type { Bowl, Ingredient } from "../../types";
+interface IngredientStore {
+  slots: Record<string, Ingredient | null>;
+  baseType: number;
+  selectedBowl: Bowl | null;
+  setBaseType: (id: number) => void;
+  setBowl: (bowl: Bowl) => void;
+  clearSelection: () => void;
+  addIngredient: (item: Ingredient) => void;
+  removeIngredient: (id: number) => void;
+}
+
+export const useIngredientStore = create<IngredientStore>()((set) => ({
+  slots: {},
+  baseType: 1,
+  selectedBowl: null,
+
+  setBaseType: (id) => set({ baseType: id }),
+
+  setBowl: (bowl) => set({ selectedBowl: bowl }),
+
+  clearSelection: () =>
+    set({
+      slots: {},
+      baseType: 1,
+      selectedBowl: null,
+    }),
+
+  addIngredient: (_item) => {},
+
+  removeIngredient: (_id) => {},
+}));
