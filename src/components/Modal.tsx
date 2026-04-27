e5dccf1543ea031baabe8ac5851c6c1beae505af
Anush707 <anushgotame333@email.com>
2026-04-27 13:58:53 +0300

    some changes on modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,11 +1,19 @@
 type Props = {
   children: React.ReactNode;
+  isOpen: boolean;
+  onClose: () => void;
 };
 
-export default function Modal({ children }: Props) {
+export default function Modal({ children, isOpen, onClose }: Props) {
+  if (!isOpen) return null;
+
   return (
-    <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
-      <div>
+    <div
+      className="fixed inset-0 bg-black/60 flex items-center justify-center z-50"
+      onClick={onClose}
+    >
+      <div className="bg-white" onClick={(e) => e.stopPropagation()}>
+        <button onClick={onClose}>X</button>
         {children}
       </div>
     </div>
